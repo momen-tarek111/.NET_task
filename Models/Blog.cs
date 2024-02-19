3da2eae8b4ec586651f539f57704e911964295f3
karas <karas@DESKTOP-46BK8T9>
2024-02-19 15:48:25 +0200

    Add project files.

---
+++ Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project3.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string AuthorName { get; set; }
+        public bool available { get; set; }
+        public int CategoryId {  get; set; }
+        [ForeignKey("CategoryId")]
+        public Category category { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Project3.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public bool EnableSize { get; set; }
+        public int CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        public Company company { get; set; }
+    }
+}
